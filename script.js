8aa0322e81414f26d5536f298fa6a7ddf3f4c925
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-02-15 10:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+document.getElementById('addTodoBtn').addEventListener('click',function(){
+	va value =document.getElementById('newTodoInput').value;
+	if(value){
+		additem(value);
+		document.getElementById('newTodoInput').value='';
+	}
+});
+function addItem(text){
+	var list =document.getElementById('todoList');
+	var item =document.createElement('li');
+	item.innerText = text;
+	list.appendChild(item);
+}
